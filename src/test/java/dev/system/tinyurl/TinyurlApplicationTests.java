0a393b526a39accbb45e754ab4b00a5e1b5096f1
Ankita Singh <37950041+ankita2503@users.noreply.github.com>
2026-09-12 13:48:06 +0200

    chore: bootstrap Spring Boot project

---
+++ src/test/java/dev/system/tinyurl/TinyurlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.system.tinyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
